4cad65c42541c39e65376087298f1a91338bcde1
Jay Al Gallenero <jeegallenero@gmail.com>
2025-03-12 09:08:15 +0800

    version 1.0.0.1

--- WinFormsApp1/WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp2/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20F);
-            label1.Location = new Point(204, 171);
+            label1.Location = new Point(229, 172);
             label1.Name = "label1";
-            label1.Size = new Size(395, 46);
+            label1.Size = new Size(329, 46);
             label1.TabIndex = 0;
-            label1.Text = "Not Updated Application";
+            label1.Text = "Updated Application";
             // 
             // Form1
             // 
